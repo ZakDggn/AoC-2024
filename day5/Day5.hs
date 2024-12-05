41c5acde918e2c88348fcf5c91d46a22282405a5
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-12-05 23:24:54 +0000

    Simplify Day 5 - only need to check for order in one direction

--- day5/Day5.hs
+++ day5/Day5.hs
@@ -1,15 +1,11 @@
 import Data.List (inits, partition, tails)
 import Data.List.Split (splitOn)
 
-type Rules = [(Int, Int)]
+type Page = Int
 
-type Update = [Int]
+type Rules = [(Page, Page)]
 
-data Direction = Forwards | Backwards
-
-tuplify :: (Show a) => [a] -> (a, a)
-tuplify [x, y] = (x, y)
-tuplify list = error $ "Couldn't tuplify list: " ++ (show list)
+type Update = [Page]
 
 parse :: String -> (Rules, [Update])
 parse contents = (rules, updates)
@@ -17,40 +13,36 @@ parse contents = (rules, updates)
     (ruleStrings, updateStrings) = tuplify . map lines . splitOn "\n\n" $ contents
     rules = map (tuplify . map read . splitOn "|") ruleStrings
     updates = map (map read . splitOn ",") updateStrings
+    tuplify [x, y] = (x, y)
 
-okPair :: Rules -> Int -> Int -> Bool
-okPair rules p1 p2 = not $ (p1, p2) `elem` rules
+okPair :: Rules -> Page -> Page -> Bool
+okPair rules p1 p2 = not $ (p2, p1) `elem` rules
 
-checkPartial :: Direction -> Rules -> Update -> Bool
-checkPartial direction rules update =
-  length update <= 1 || case direction of
-    Forwards -> check update flip
-    Backwards -> check (reverse update) id
+checkPartial :: Rules -> Update -> Bool
+checkPartial rules update =
+  length update <= 1 || check update
   where
-    check (p : ps) order = and $ map (order (okPair rules) p) ps
+    check (p : ps) = and $ map (okPair rules p) ps
 
 isCorrect :: Rules -> Update -> Bool
-isCorrect rules update = isCorrectForwards && isCorrectBackwards
-  where
-    isCorrectForwards = and . map (checkPartial Forwards rules) $ tails update
-    isCorrectBackwards = and . map (checkPartial Backwards rules) $ inits update
+isCorrect rules update = and . map (checkPartial rules) $ tails update
 
-getMiddle :: Update -> Int
+getMiddle :: Update -> Page
 getMiddle update = update !! ((length update) `div` 2)
 
 part1 :: [Update] -> Int
 part1 = sum . map getMiddle
 
-correctOrder :: Rules -> Update -> Update
-correctOrder rules = foldr insert []
+sort :: Rules -> Update -> Update
+sort rules = foldr insert []
   where
     insert page [] = [page]
     insert page (p : ps)
-      | okPair rules page p = p : (insert page ps)
-      | otherwise = page : p : ps
+      | okPair rules page p = page : p : ps
+      | otherwise = p : (insert page ps)
 
 part2 :: Rules -> [Update] -> Int
-part2 rules = sum . map getMiddle . map (correctOrder rules)
+part2 rules = sum . map getMiddle . map (sort rules)
 
 main = do
   contents <- readFile "input"

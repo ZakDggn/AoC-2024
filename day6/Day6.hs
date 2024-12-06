663b56b399959cc1f9a624758aa00a8e015b6a56
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-12-06 16:46:05 +0000

    Day 6 - optimise part 1 by using a set for visited cells

--- day6/Day6.hs
+++ day6/Day6.hs
@@ -1,16 +1,18 @@
-import Debug.Trace
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 data Direction = North | East | South | West deriving (Show, Enum)
 
+type Grid = [String]
 type Position = (Int, Int)
 
-parse :: String -> ([String], Position)
+parse :: String -> (Grid, Position)
 parse contents = (grid, pos)
   where
     gridWithGuard = lines contents
     maxY = length gridWithGuard - 1
     maxX = length (gridWithGuard !! 0) - 1
-    pos = head [(x, y) | x <- [0 .. maxX], y <- [0 .. maxY], getCell gridWithGuard (x, y) == '^']
+    pos = head [(x, y) | x <- [0 .. maxX], y <- [0 .. maxY], gridWithGuard !! y !! x == '^']
     grid = map (map removeGuard) gridWithGuard
     removeGuard c = if c == '^' then '.' else c
 
@@ -18,17 +20,17 @@ rotate :: Direction -> Direction
 rotate West = North
 rotate direction = succ direction
 
-getCell :: [String] -> (Int, Int) -> Char
+getCell :: Grid -> (Int, Int) -> Char
 getCell grid (x, y) = grid !! y !! x
 
-step :: [String] -> Position -> Direction -> ([String], Maybe Position, Direction)
+step :: Grid -> Position -> Direction -> (Maybe Position, Direction)
 step grid pos direction
-  | isOutOfBounds pos' = (grid', Nothing, direction)
-  | isObstacle = (grid, Just pos, rotate direction)
-  | otherwise = (grid', Just pos', direction)
+  | isOutOfBounds pos' = (Nothing, direction)
+  | isObstacle pos' = (Just pos, rotate direction)
+  | otherwise = (Just pos', direction)
   where
-    (x, y) = pos
-    pos' = (x + dx, y + dy)
+    (posX, posY) = pos
+    pos' = (posX + dx, posY + dy)
     (dx, dy) = case direction of
       North -> (0, -1)
       East -> (1, 0)
@@ -36,22 +38,17 @@ step grid pos direction
       West -> (-1, 0)
     maxY = length grid - 1
     maxX = length (grid !! 0) - 1
-    grid' = [[char' | x <- [0 .. maxX], let char' = if (x, y) == pos then 'X' else getCell grid (x, y)] | y <- [0 .. maxY]]
-    isObstacle = getCell grid pos' == '#'
     isOutOfBounds (x, y) = x < 0 || x > maxX || y < 0 || y > maxY
+    isObstacle (x, y) = getCell grid (x, y) == '#'
 
-countVisits :: [String] -> Int
-countVisits grid = length visited
+part1 :: Grid -> Position -> Int
+part1 grid startPos = Set.size $ getVisited (Just startPos, North)
   where
-    maxY = length grid - 1
-    maxX = length (grid !! 0) - 1
-    visited = [() | x <- [0 .. maxX], y <- [0 .. maxY], getCell grid (x, y) == 'X']
-
-part1 :: ([String], Maybe Position, Direction) -> Int
-part1 (grid, Nothing, _) = countVisits grid
-part1 (grid, (Just pos), direction) = trace (unlines grid ++ "\n" ++ show pos ++ ", " ++ show direction ++ "\n") part1 $ step grid pos direction
+    getVisited (pos, direction) = case pos of
+      Nothing -> Set.empty
+      Just pos' -> Set.insert pos' $ getVisited $ step grid pos' direction
 
 main = do
   contents <- readFile "input"
   let (grid, pos) = parse contents
-  print $ part1 (grid, Just pos, North)
+  print $ part1 grid pos

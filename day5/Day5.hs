6b1eb354601a900323bf82113e6be4db5482d16e
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-12-06 10:03:23 +0000

    Optimise Day 5 - replace association list with map

--- day5/Day5.hs
+++ day5/Day5.hs
@@ -1,22 +1,27 @@
 import Data.List (inits, partition, tails)
 import Data.List.Split (splitOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 type Page = Int
 
-type Rules = [(Page, Page)]
+type Rules = Map Page [Page]
 
 type Update = [Page]
 
 parse :: String -> (Rules, [Update])
 parse contents = (rules, updates)
   where
-    (ruleStrings, updateStrings) = tuplify . map lines . splitOn "\n\n" $ contents
-    rules = map (tuplify . map read . splitOn "|") ruleStrings
+    [ruleStrings, updateStrings] = map lines . splitOn "\n\n" $ contents
+    rulesList = map (tuplify . map read . splitOn "|") ruleStrings
+    rules = Map.fromListWith (++) rulesList
     updates = map (map read . splitOn ",") updateStrings
-    tuplify [x, y] = (x, y)
+    tuplify [x, y] = (x, [y])
 
 okPair :: Rules -> Page -> Page -> Bool
-okPair rules p1 p2 = not $ (p2, p1) `elem` rules
+okPair rules p1 p2 = case (Map.lookup p2 rules) of
+  Just ps -> not $ p1 `elem` ps
+  Nothing -> True
 
 checkPartial :: Rules -> Update -> Bool
 checkPartial rules update =

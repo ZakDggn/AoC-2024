175929477dddf394ee8e0a61c09e565166864a41
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-12-06 17:25:33 +0000

    Day 6 - use obstacle map to optimise part 1 and prepare for part 2

--- day6/Day6.hs
+++ day6/Day6.hs
@@ -1,33 +1,35 @@
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 
 data Direction = North | East | South | West deriving (Show, Enum)
 
-type Grid = [String]
 type Position = (Int, Int)
 
-parse :: String -> (Grid, Position)
-parse contents = (grid, pos)
+type Obstacles = Map Position [Direction]
+
+type Dimensions = (Int, Int)
+
+parse :: String -> (Obstacles, Position, Dimensions)
+parse contents = (obstacles, pos, (rows, cols))
   where
-    gridWithGuard = lines contents
-    maxY = length gridWithGuard - 1
-    maxX = length (gridWithGuard !! 0) - 1
-    pos = head [(x, y) | x <- [0 .. maxX], y <- [0 .. maxY], gridWithGuard !! y !! x == '^']
-    grid = map (map removeGuard) gridWithGuard
-    removeGuard c = if c == '^' then '.' else c
+    grid = lines contents
+    rows = length grid
+    cols = length (grid !! 0)
+    maxY = rows - 1
+    maxX = cols - 1
+    pos = head [(x, y) | x <- [0 .. maxX], y <- [0 .. maxY], grid !! y !! x == '^']
+    obstacles = Map.fromList [((x, y), []) | x <- [0 .. maxX], y <- [0 .. maxY], grid !! y !! x == '#']
 
 rotate :: Direction -> Direction
 rotate West = North
 rotate direction = succ direction
 
-getCell :: Grid -> (Int, Int) -> Char
-getCell grid (x, y) = grid !! y !! x
-
-step :: Grid -> Position -> Direction -> (Maybe Position, Direction)
-step grid pos direction
-  | isOutOfBounds pos' = (Nothing, direction)
-  | isObstacle pos' = (Just pos, rotate direction)
-  | otherwise = (Just pos', direction)
+step :: Obstacles -> Position -> Direction -> (Position, Direction)
+step obstacles pos direction
+  | isObstacle pos' = (pos, rotate direction)
+  | otherwise = (pos', direction)
   where
     (posX, posY) = pos
     pos' = (posX + dx, posY + dy)
@@ -36,19 +38,20 @@ step grid pos direction
       East -> (1, 0)
       South -> (0, 1)
       West -> (-1, 0)
-    maxY = length grid - 1
-    maxX = length (grid !! 0) - 1
-    isOutOfBounds (x, y) = x < 0 || x > maxX || y < 0 || y > maxY
-    isObstacle (x, y) = getCell grid (x, y) == '#'
+    isObstacle (x, y) = Map.member (x, y) obstacles
 
-part1 :: Grid -> Position -> Int
-part1 grid startPos = Set.size $ getVisited (Just startPos, North)
+getVisited :: Obstacles -> Dimensions -> (Position, Direction) -> Set Position
+getVisited obstacles (rows, cols) (pos, direction)
+  | isOutOfBounds pos' = Set.singleton pos
+  | otherwise = Set.insert pos $ getVisited obstacles (rows, cols) (pos', direction')
   where
-    getVisited (pos, direction) = case pos of
-      Nothing -> Set.empty
-      Just pos' -> Set.insert pos' $ getVisited $ step grid pos' direction
+    (pos', direction') = step obstacles pos direction
+    isOutOfBounds (x, y) = x < 0 || x >= cols || y < 0 || y >= rows
+
+part1 :: Obstacles -> Dimensions -> Position -> Int
+part1 obstacles dimensions startPos = Set.size $ getVisited obstacles dimensions (startPos, North)
 
 main = do
   contents <- readFile "input"
-  let (grid, pos) = parse contents
-  print $ part1 grid pos
+  let (obstacles, pos, dimensions) = parse contents
+  print $ part1 obstacles pos dimensions

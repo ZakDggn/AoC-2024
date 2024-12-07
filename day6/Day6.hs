d330ed4a1dfc9d97f73e0b6d93ebc986510b670c
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-12-07 02:22:47 +0000

    Day 6 - part 2 failed attempt

--- day6/Day6.hs
+++ day6/Day6.hs
@@ -1,9 +1,9 @@
-import Data.Map (Map)
+import Data.Map (Map, (!))
 import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-data Direction = North | East | South | West deriving (Show, Enum)
+data Direction = North | East | South | West deriving (Eq, Enum, Show)
 
 type Position = (Int, Int)
 
@@ -26,32 +26,73 @@ rotate :: Direction -> Direction
 rotate West = North
 rotate direction = succ direction
 
+getDelta :: Direction -> (Int, Int)
+getDelta North = (0, -1)
+getDelta East = (1, 0)
+getDelta South = (0, 1)
+getDelta West = (-1, 0)
+
+nextPos :: Position -> Direction -> Position
+nextPos (x, y) direction = (x + dx, y + dy)
+  where
+    (dx, dy) = getDelta direction
+
 step :: Obstacles -> Position -> Direction -> (Position, Direction)
 step obstacles pos direction
   | isObstacle pos' = (pos, rotate direction)
   | otherwise = (pos', direction)
   where
-    (posX, posY) = pos
-    pos' = (posX + dx, posY + dy)
-    (dx, dy) = case direction of
-      North -> (0, -1)
-      East -> (1, 0)
-      South -> (0, 1)
-      West -> (-1, 0)
+    pos' = nextPos pos direction
     isObstacle (x, y) = Map.member (x, y) obstacles
 
+isOutOfBounds :: Dimensions -> Position -> Bool
+isOutOfBounds (rows, cols) (x, y) = x < 0 || x >= cols || y < 0 || y >= rows
+
 getVisited :: Obstacles -> Dimensions -> (Position, Direction) -> Set Position
-getVisited obstacles (rows, cols) (pos, direction)
-  | isOutOfBounds pos' = Set.singleton pos
-  | otherwise = Set.insert pos $ getVisited obstacles (rows, cols) (pos', direction')
+getVisited obstacles dimensions (pos, direction)
+  | isOutOfBounds dimensions pos' = Set.singleton pos
+  | otherwise = Set.insert pos visited
   where
+    visited = getVisited obstacles dimensions (pos', direction')
     (pos', direction') = step obstacles pos direction
-    isOutOfBounds (x, y) = x < 0 || x >= cols || y < 0 || y >= rows
 
 part1 :: Obstacles -> Dimensions -> Position -> Int
 part1 obstacles dimensions startPos = Set.size $ getVisited obstacles dimensions (startPos, North)
 
+wouldLoop :: Obstacles -> Dimensions -> Position -> Direction -> Bool
+wouldLoop obstacles dimensions pos searchDirection
+  | isOutOfBounds dimensions pos = False
+  | foundObstacle = isLoopingObstacle
+  | otherwise = wouldLoop obstacles dimensions pos' searchDirection
+  where
+    (pos', direction') = step obstacles pos searchDirection
+    foundObstacle = direction' /= searchDirection
+    obstaclePos = nextPos pos searchDirection
+    -- TODO: might be a looping obstacle even if not visited yet?
+    isLoopingObstacle = searchDirection `elem` (obstacles ! obstaclePos)
+
+getLoops :: Obstacles -> Dimensions -> (Position, Direction) -> [Position]
+getLoops obstacles dimensions (pos, direction)
+  | isOutOfBounds dimensions pos' = []
+  | otherwise = loopPositions'
+  where
+    (pos', direction') = step obstacles pos direction
+    loopPositions = getLoops obstacles' dimensions (pos', direction')
+    obstacles' =
+      if direction /= direction'
+        then Map.adjust (direction :) (nextPos pos direction) obstacles
+        else obstacles
+    searchDirection = rotate direction
+    loopPositions' =
+      if wouldLoop obstacles' dimensions pos searchDirection
+        then (nextPos pos direction) : loopPositions
+        else loopPositions
+
+part2 :: Obstacles -> Dimensions -> Position -> Int
+part2 obstacles dimensions startPos = length $ getLoops obstacles dimensions (startPos, North)
+
 main = do
   contents <- readFile "input"
   let (obstacles, pos, dimensions) = parse contents
-  print $ part1 obstacles pos dimensions
+  print $ part1 obstacles dimensions pos
+  print $ part2 obstacles dimensions pos

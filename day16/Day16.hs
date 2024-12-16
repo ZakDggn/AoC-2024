1cd87e28c32193e599b9522a7506e40b732c3f37
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-12-16 22:52:45 +0000

    Day 16 - part 1 using BFS

--- day16/Day16.hs
+++ day16/Day16.hs
@@ -1,69 +1,82 @@
-import Data.Array
-import Data.Maybe (catMaybes, fromJust)
-import Data.Set (Set)
-import qualified Data.Set as Set
-
-type Grid = Array Position Char
+import Data.List (nub, partition)
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 type Position = (Int, Int)
 
 type Score = Int
 
-data Direction = North | South | West | East deriving (Show)
+data Direction = North | East | South | West deriving (Enum, Eq, Ord)
 
-parse :: String -> Grid
-parse contents = listArray ((0, 0), (maxY, maxX)) $ concat gridLines
+type Maze = Map Position (Map Direction Score)
+
+type CellState = (Position, Direction, Score)
+
+parse :: String -> (Maze, Position, Position)
+parse contents = (Map.fromList maze, (maxY - 1, 1), (1, maxX - 1))
   where
-    gridLines = lines contents
-    maxY = length gridLines - 1
-    maxX = length (gridLines !! 0) - 1
+    grid = lines contents
+    maxY = length grid - 1
+    maxX = length (grid !! 0) - 1
+    maze = [((y, x), Map.empty) | y <- [0 .. maxY], x <- [0 .. maxX], grid !! y !! x /= '#']
 
 turnLeft :: Direction -> Direction
 turnLeft North = West
-turnLeft South = East
-turnLeft West = South
-turnLeft East = North
+turnLeft direction = pred direction
 
 turnRight :: Direction -> Direction
-turnRight North = East
-turnRight South = West
 turnRight West = North
-turnRight East = South
+turnRight direction = succ direction
 
 delta :: Direction -> Position
 delta North = (-1, 0)
+delta East = (0, 1)
 delta South = (1, 0)
 delta West = (0, -1)
-delta East = (0, 1)
 
 nextPos :: Position -> Direction -> Position
 nextPos (y, x) direction = let (dy, dx) = delta direction in (y + dy, x + dx)
 
-search :: Grid -> Position -> Direction -> Set Position -> Maybe Score
-search grid pos direction visited
-  | cell == '#' = Nothing
-  | cell == 'E' = Just 0
-  | pos `elem` visited = Nothing
-  | otherwise = score
+nextCells :: CellState -> [CellState]
+nextCells (pos, direction, score) = [forward, left, right]
   where
-    cell = grid ! pos
-    visited' = Set.insert pos visited
-    scoreF = (+ 1) <$> search grid (nextPos pos direction) direction visited'
-    posLeft = nextPos pos (turnLeft direction)
-    scoreL = (+ 1001) <$> search grid posLeft (turnLeft direction) visited'
-    posRight = nextPos pos (turnRight direction)
-    scoreR = (+ 1001) <$> search grid posRight (turnRight direction) visited'
-    score = case catMaybes [scoreF, scoreL, scoreR] of
-      [] -> Nothing
-      scores -> Just $ minimum scores
+    forward = (nextPos pos direction, direction, score + 1)
+    left = (pos, turnLeft direction, score + 1000)
+    right = (pos, turnRight direction, score + 1000)
 
-part1 :: Grid -> Score
-part1 grid = fromJust $ search grid start East Set.empty
+betterThanCurrent :: Maze -> CellState -> Bool
+betterThanCurrent maze (pos, direction, score) = case Map.lookup pos maze of
+  Nothing -> False
+  Just scoreMap -> case Map.lookup direction scoreMap of
+    Nothing -> True
+    Just bestScore -> if score < bestScore then True else False
+
+updateMaze :: CellState -> Maze -> Maze
+updateMaze cell@(pos, direction, score) maze
+  | betterThanCurrent maze cell = Map.adjust (Map.insert direction score) pos maze
+  | otherwise = maze
+
+step :: Maze -> [CellState] -> (Maze, [CellState])
+step maze cells = (maze', adjacents')
   where
-    (maxY, _) = snd $ bounds grid
-    start = (maxY - 1, 1)
+    maze' = foldr updateMaze maze cells
+    adjacents = concatMap nextCells cells
+    adjacents' = nub $ filter (betterThanCurrent maze) adjacents
+
+search :: Maze -> [CellState] -> Position -> [Score] -> [Score]
+search _ [] _ scores = scores
+search maze cells end scores = search maze' cells' end scores'
+  where
+    (ends, unvisited) = partition ((== end) . getPosition) cells
+    scores' = scores ++ (map getScore ends)
+    (maze', cells') = step maze unvisited
+    getPosition (pos, _, _) = pos
+    getScore (_, _, score) = score
+
+part1 :: Maze -> Position -> Position -> Score
+part1 maze start end = minimum $ search maze [(start, East, 0)] end []
 
 main = do
-  contents <- readFile "example1"
-  let grid = parse contents
-  print $ part1 grid
+  contents <- readFile "input"
+  let (maze, start, end) = parse contents
+  print $ part1 maze start end

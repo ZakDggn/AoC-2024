587f28dfbcc003020a9b12e16b52ff72a5c1d8e2
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-12-22 16:25:26 +0000

    Day 22 - part 2

--- day22/Day22.hs
+++ day22/Day22.hs
@@ -1,4 +1,6 @@
 import Data.Bits (xor)
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 parse :: String -> [Int]
 parse = map read . lines
@@ -11,6 +13,29 @@ next = evolve (* 2048) . evolve (`div` 32) . evolve (* 64)
 part1 :: [Int] -> Int
 part1 = sum . map ((!! 2000) . iterate next)
 
+prices :: Int -> [Int]
+prices = map (`mod` 10) . take 2000 . iterate next
+
+changes :: [Int] -> [Int]
+changes ps = zipWith (-) (drop 1 ps) ps
+
+seqsOfFour :: [a] -> [(a, a, a, a)]
+seqsOfFour (w : x : y : z : rest) = (w, x, y, z) : seqsOfFour (x : y : z : rest)
+seqsOfFour _ = []
+
+makeSeqToPrice :: Int -> Map (Int, Int, Int, Int) Int
+makeSeqToPrice secret = Map.fromListWith (flip const) $ zip seqs (drop 4 ps)
+  where
+    ps = prices secret
+    seqs = seqsOfFour $ changes ps
+
+part2 :: [Int] -> Int
+part2 secrets = maximum $ Map.elems seqToTotalPrice
+  where
+    seqToPrices = map makeSeqToPrice secrets
+    seqToTotalPrice = foldr1 (Map.unionWith (+)) seqToPrices
+
 main = do
   secrets <- parse <$> readFile "input"
   print $ part1 secrets
+  print $ part2 secrets
